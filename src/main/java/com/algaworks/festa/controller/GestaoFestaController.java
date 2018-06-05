3bdaffef465a7ed6439767a6ca747932d401cbc4
Mauricio Amorim <mjvamorim@gmail.com>
2018-06-05 20:41:49 -0300

    Layout Completo

---
+++ src/main/java/com/algaworks/festa/controller/GestaoFestaController.java
@@ -0,0 +1,20 @@
+package com.algaworks.festa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class GestaoFestaController {
+
+	@RequestMapping("")
+	public ModelAndView home() {
+		ModelAndView mv = new ModelAndView("Home");
+		return (mv);
+	}
+
+
+	
+
+}
